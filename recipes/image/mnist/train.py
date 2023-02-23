5c27137c2f424dfb16046aa3ccc525c1a8abc385
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2023-02-23 21:47:52 +0000

    prepare model for ray hp tuning by adding hp input as dict

--- recipes/image/mnist/train.py
+++ recipes/image/mnist/train.py
@@ -4,6 +4,7 @@ from omegaconf import DictConfig, OmegaConf
 import hydra
 from hydra.utils import instantiate
 import wandb
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
 
 @hydra.main(version_base="1.3",config_path="conf", config_name="train.yaml")
 def main(cfg: DictConfig) -> None:
@@ -24,11 +25,16 @@ def main(cfg: DictConfig) -> None:
     callbacks = []
     for _, cb_conf in cfg.callbacks.items():
         callbacks.append(instantiate(cb_conf))
+    
+    # ray tune callback
+    metrics = {"loss": "val/avg_loss", "acc": "val/avg_accuracy"}
+    tune_cb = TuneReportCallback(metrics, on="validation_end")
+    callbacks.append(tune_cb)
 
-    # logger = instantiate(cfg.logger)
+    logger = instantiate(cfg.logger)
 
-    profiler = instantiate(cfg.profiler)
-    trainer = instantiate(cfg.trainer, callbacks=callbacks) #, profiler=profiler) #, logger=[logger])
+    # profiler = instantiate(cfg.profiler)
+    trainer = instantiate(cfg.trainer, callbacks=callbacks, logger=[logger]) #, profiler=profiler) #, logger=[logger])
 
     if cfg.get("train"):
         # trainer.fit(model=autoencoder_pl, train_dataloaders=train_dl, val_dataloaders=dev_dl, ckpt_path=cfg.get("ckpt_path"))
