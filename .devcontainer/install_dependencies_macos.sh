f4f2ee7bbb641cb5c41cee1b89bd185fc31baded
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2023-09-28 14:36:47 -0700

    update setup instructions

--- .devcontainer/install_dependencies_macos.sh
+++ .devcontainer/install_dependencies_macos.sh
@@ -13,3 +13,7 @@ sudo make install
 
 # SPACY
 python -m spacy download en_core_web_sm
+
+# PHONEMIZER
+brew install espeak
+export PHONEMIZER_ESPEAK_LIBRARY=/opt/homebrew/Cellar/espeak/1.48.04_1/lib/libespeak.dylib
