505f6ef8d148cbdb1b0737ec37a08bb81ae0257b
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2023-10-14 16:41:57 -0700

    infer example

---
+++ recipes/image/mnist/infer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from nimrod.models.mlp import MLP_PL
+from omegaconf import OmegaConf
+from hydra.utils import instantiate 
+import pytorch_lightning as pl
+import torch
+
+# checkpoints
+PATH = "logs/runs/2023-10-11_21-50-57/checkpoints/last.ckpt"
+model = MLP_PL.load_from_checkpoint(PATH).to(torch.device('cpu'))
+
+# fake batch data 
+n_batch, n_channel, w, h = 2, 1, 28,28
+x = torch.rand((n_batch, n_channel, w*h))
+
+# straight pytorch
+model.eval()
+with torch.no_grad():
+    # model forward method calls mlp which is (B,C,W*H) unlike datamodule which is (B,C,W,H)
+    y_hat = model(x).argmax(dim=2)
+print(y_hat)
+
+# datamodule
+cfg = OmegaConf.load('../../../config/data/image/mnist.yaml')
+datamodule = instantiate(cfg.datamodule)
+datamodule.prepare_data()
+datamodule.setup()
+
+# with trainer predict
+trainer = pl.Trainer()
+preds = trainer.predict(model, datamodule.test_dataloader())
+
