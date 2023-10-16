67ba0fe41952acb995f5ea5564829591b5e8e0e8
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2023-10-16 12:58:31 -0700

    shortcut script to train on mac

--- recipes/image/mnist/train.py
+++ recipes/image/mnist/train.py
@@ -6,7 +6,7 @@ from hydra.utils import instantiate
 import wandb
 
 @hydra.main(version_base="1.3",config_path="conf", config_name="train.yaml")
-def main(cfg: DictConfig) -> None:
+def main(cfg: DictConfig) -> dict:
 
     # PARAMS
     # print(OmegaConf.to_yaml(cfg))
@@ -27,6 +27,8 @@ def main(cfg: DictConfig) -> None:
 
     logger = instantiate(cfg.logger)
     if isinstance(logger, pl.loggers.wandb.WandbLogger):
+        # deal with hangs when hp optim multirun training 
+        wandb.init(settings=wandb.Settings(start_method="thread"))
         # wandb requires dict not DictConfig
         hp = OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
         logger.experiment.config.update(hp)
@@ -37,13 +39,17 @@ def main(cfg: DictConfig) -> None:
     if cfg.get("train"):
         # trainer.fit(model=autoencoder_pl, train_dataloaders=train_dl, val_dataloaders=dev_dl, ckpt_path=cfg.get("ckpt_path"))
         trainer.fit(model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+    train_metrics = trainer.callback_metrics
 
     # TEST
     if cfg.get("test"):
         # trainer.test(autoencoder_pl, dataloaders=test_dl)
         trainer.test(datamodule=datamodule, ckpt_path="best")
+    test_metrics = trainer.callback_metrics
 
     wandb.finish()
+    metric_dict = {**train_metrics, **test_metrics}
+    return metric_dict[cfg.get("optimized_metric")]
 
 if __name__ == "__main__":
     main()
