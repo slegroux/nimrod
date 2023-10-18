1c92310d1905b49e667cd94924862aa2d358885e
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2023-10-17 20:15:23 -0700

    add local, cloud checkpointing and inference

--- recipes/image/mnist/infer.py
+++ recipes/image/mnist/infer.py
@@ -4,27 +4,41 @@ from omegaconf import OmegaConf
 from hydra.utils import instantiate 
 import pytorch_lightning as pl
 import torch
+import wandb
+from pathlib import Path
 
-# checkpoints
-PATH = "logs/runs/2023-10-11_21-50-57/checkpoints/last.ckpt"
-model = MLP_PL.load_from_checkpoint(PATH).to(torch.device('cpu'))
 
-# fake batch data 
-n_batch, n_channel, w, h = 2, 1, 28,28
-x = torch.rand((n_batch, n_channel, w*h))
-
-# straight pytorch
-model.eval()
-with torch.no_grad():
-    # model forward method calls mlp which is (B,C,W*H) unlike datamodule which is (B,C,W,H)
-    y_hat = model(x).argmax(dim=2)
-print(y_hat)
-
-# datamodule
-cfg = OmegaConf.load('../../../config/data/image/mnist.yaml')
-datamodule = instantiate(cfg.datamodule)
+# instantiate from configs
+cfg = OmegaConf.load('conf/train.yaml')
+model = instantiate(cfg.model)
+# default train.yaml is setup for ...training! change num_workers for predict
+datamodule = instantiate(cfg.datamodule, num_workers=0, persistent_workers=False)
 datamodule.prepare_data()
 datamodule.setup()
+
+
+# local checkpoints
+PATH = "logs/multiruns/2023-10-17_17-37-15/5/checkpoints/last.ckpt"
+model = MLP_PL.load_from_checkpoint(PATH).to(torch.device('cpu'))
+
+# WANDB to retrive checkpoints from cloud
+# run = wandb.init()
+# artifact = run.use_artifact('slegroux/MNIST-HP/model-girtmnkf:v0', type='model')
+# artifact_dir = artifact.download()
+# model = MLP_PL.load_from_checkpoint(Path(artifact_dir) / "model.ckpt").to(torch.device('cpu'))
+artifact_dir = "artifacts/model-girtmnkf:v0"
+model = MLP_PL.load_from_checkpoint(Path(artifact_dir) / "model.ckpt").to(torch.device('cpu'))
+
+# # fake batch data 
+# n_batch, n_channel, w, h = 2, 1, 28,28
+# x = torch.rand((n_batch, n_channel, w*h))
+
+# # straight pytorch
+# model.eval()
+# with torch.no_grad():
+#     # model forward method calls mlp which is (B,C,W*H) unlike datamodule which is (B,C,W,H)
+#     y_hat = model(x).argmax(dim=2)
+# print(y_hat)
 
 # with trainer predict
 trainer = pl.Trainer()
--- recipes/image/mnist/optuna_sweep.sh
+++ recipes/image/mnist/optuna_sweep.sh
@@ -3,8 +3,10 @@
 # optuna hp tuning
 # python train.py --multirun project='mnist-hp' trainer.max_epochs=5
 # python train.py --multirun project='mnist-hp' trainer.max_epochs=10 datamodule.batch_size=64,128,256 'model.lr=tag(log, interval(0.001, 1))' 'model.dropout=interval(0,1)'
-python train.py --multirun project='mnist-hp' trainer.max_epochs=2 logger.group='sweepin'
-
+python train.py --multirun project='mnist-hp' \
+    logger.group='mac-sweepin-all' logger.log_model='all' \
+    trainer.max_epochs=2 \
+    model.dropout=0.31 model.n_h=128
 
 # show optuna training params
 # python train.py hydra/sweeper=optuna --cfg hydra #-p hydra.sweeper
