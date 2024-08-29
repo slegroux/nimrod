d5666772f4722e679794120f02403b5527b05b4d
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2024-08-28 21:00:44 -0700

    add startup command to install deps when instance boots

--- .devcontainer/command.sh
+++ .devcontainer/command.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+# -u 0 gives root access to the container
+# docker run -it -u 0 --rm bitnami/pytorch:2.3.1 /bin/bash
 
+apt-get update
+apt-get install -y --no-install-recommends espeak-ng \
+    espeak-ng \
+    git-lfs \
+    libsndfile-dev \
+    curl 
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+# install nimrod
 git clone https://github.com/slegroux/nimrod
-# jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+cd nimrod
+pip install -e .
+# alternative install from python package
+# pip install slg-nimrod==0.0.11
+
+# spacy requirements for en
+python -m spacy download en_core_web_sm
+# nbdev stuff
+nbdev_install_hooks && nbdev_install_quarto
+# prevent some cloud GPU providers to automatically start tmux on login
+touch ~/.no_auto_tmux;
